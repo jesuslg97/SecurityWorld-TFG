3e919f57891b3af683d19c0dabcf235da6fd1da5
jesus <jelopezgonzalez@gmail.com>
2021-11-27 19:24:54 +0000

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
   get 'home/index'
+
+  devise_for :users
 
   #get 'categories/:id', to: 'categories#index', as: :category
   #get 'categories/:id', to: 'categories#show', as: :show_category
