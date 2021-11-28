56e3a312015436fe62d30bc048a23717af48e72a
jesus <jelopezgonzalez@gmail.com>
2021-11-28 23:04:23 +0000

    CRUD articles finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,55 @@
 class ArticlesController < ApplicationController
 
+  before_action :find_article, only: %i[ show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
     @attacks = Attack.all
     @questions = Question.all
     @generals = General.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(strong_params_article)
+    if @article.save
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(strong_params_article)
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def strong_params_article
+    params.require(:article).permit(:nombre, :descripcion, :texto, :category_id)
+  end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,9 @@ class Article < ApplicationRecord
   has_many :questions
   has_many :generals
 
+  validates :nombre, presence: true
+  validates :descripcion, presence: true, length: { maximum: 1000 }
+  validates :texto, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true
+
 end
