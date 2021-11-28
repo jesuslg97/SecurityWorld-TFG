17c98aceb85823f811027adee96c549d8a87fe03
jesus <jelopezgonzalez@gmail.com>
2021-11-28 23:35:54 +0000

    CRUD attacks finished

--- app/controllers/attacks_controller.rb
+++ app/controllers/attacks_controller.rb
@@ -1,12 +1,53 @@
 class AttacksController < ApplicationController
 
+  before_action :find_attack, only: %i[ show edit update destroy]
+
   def index
     @attacks = Attack.all
   end
 
   def show
-    @attacks = Attack.find(params[:id])
+    @attack = Attack.find(params[:id])
     @contents = Content.all
+  end
+
+  def new
+    @attack = Attack.new
+  end
+
+  def edit
+  end
+
+  def create
+    @attack = Attack.new(strong_params_attack)
+    if @attack.save
+      redirect_to attacks_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @attack.update(strong_params_attack)
+      redirect_to attacks_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @attack.destroy
+    redirect_to attacks_path
+  end
+
+  private
+
+  def find_attack
+    @attack = Attack.find(params[:id])
+  end
+
+  def strong_params_attack
+    params.require(:attack).permit(:nombre, :informacion, :article_id)
   end
 
 end
--- app/models/attack.rb
+++ app/models/attack.rb
@@ -3,4 +3,8 @@ class Attack < ApplicationRecord
   belongs_to :article
   has_many :contents
 
+  validates :nombre, presence: true
+  validates :informacion, presence: true, length: { maximum: 1000 }
+  validates :article_id, presence: true
+
 end
