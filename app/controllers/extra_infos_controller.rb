3cac1bbc4483aecc3f2f6c9e7c90f85212df9388
jesus <jelopezgonzalez@gmail.com>
2021-12-01 12:36:39 +0000

    CRUD extra_infos finished

--- app/controllers/extra_infos_controller.rb
+++ app/controllers/extra_infos_controller.rb
@@ -1,12 +1,53 @@
 class ExtraInfosController < ApplicationController
 
+  before_action :find_extra_info, only: %i[ show edit update destroy]
+
   def index
     @extra_infos = ExtraInfo.all
+    @extra = Extra.find(params[:id])
   end
 
   def show
-    @extra_infos = ExtraInfo.find(params[:id])
     @extras = Extra.all
+  end
+
+  def new
+    @extra_info = ExtraInfo.new
+  end
+
+  def edit
+  end
+
+  def create
+    @extra_info = ExtraInfo.new(strong_params_extra_info)
+    if @extra_info.save
+      redirect_to extras_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @extra_info.update(strong_params_extra_info)
+      redirect_to extras_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @extra_info.destroy
+    redirect_to extras_path
+  end
+
+  private
+
+  def find_extra_info
+    @extra_info = ExtraInfo.find(params[:id])
+  end
+
+  def strong_params_extra_info
+    params.require(:extra_info).permit(:nombre, :descripcion, :texto, :extra_id, :image)
   end
 
 end
--- app/models/extra_info.rb
+++ app/models/extra_info.rb
@@ -2,4 +2,11 @@ class ExtraInfo < ApplicationRecord
 
   belongs_to :extra
 
+  has_one_attached :image
+  validates :nombre, presence: true
+  validates :descripcion, presence: true, length: { maximum: 3000 }
+  validates :texto, presence: true, length: { maximum: 1000 }
+  validates :extra_id, presence: true
+  #validates :image
+
 end
