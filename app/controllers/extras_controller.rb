2e24c9bb5640c844cb5430b667accf8ee20ef835
jesus <jelopezgonzalez@gmail.com>
2021-12-01 11:34:45 +0000

    CRUD extras finished

--- app/controllers/extras_controller.rb
+++ app/controllers/extras_controller.rb
@@ -1,12 +1,52 @@
 class ExtrasController < ApplicationController
 
+  before_action :find_extra, only: %i[ show edit update destroy]
+
   def index
-    @extra = Extra.find(params[:id])
+    @extras = Extra.all
   end
 
   def show
-    @extra = Extra.find(params[:id])
     @extra_infos = ExtraInfo.all
+  end
+
+  def new
+    @extra = Extra.new
+  end
+
+  def edit
+  end
+
+  def create
+    @extra = Extra.new(strong_params_extra)
+    if @extra.save
+      redirect_to extras_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @extra.update(strong_params_extra)
+      redirect_to extras_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @extra.destroy
+    redirect_to extras_path
+  end
+
+  private
+
+  def find_extra
+    @extra = Extra.find(params[:id])
+  end
+
+  def strong_params_extra
+    params.require(:extra).permit(:nombre, :descripcion, :image)
   end
 
 end
--- app/models/extra.rb
+++ app/models/extra.rb
@@ -2,4 +2,9 @@ class Extra < ApplicationRecord
 
   has_many :extra_infos
 
+  has_one_attached :image
+  validates :nombre, presence: true
+  validates :descripcion, presence: true, length: { maximum: 1000 }
+  #validates :image
+
 end
